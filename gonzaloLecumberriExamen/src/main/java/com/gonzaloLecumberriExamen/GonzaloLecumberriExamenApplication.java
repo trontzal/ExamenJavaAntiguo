59f85a6cf3503fed0d7361103fc9050c8fab0b30
Curso <Curso@Gorbea01.ipartekaula.com>
2024-01-08 13:14:51 +0100

    creo el proyecto spring

---
+++ gonzaloLecumberriExamen/src/main/java/com/gonzaloLecumberriExamen/GonzaloLecumberriExamenApplication.java
@@ -0,0 +1,13 @@
+package com.gonzaloLecumberriExamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonzaloLecumberriExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonzaloLecumberriExamenApplication.class, args);
+	}
+
+}
---
+++ gonzaloLecumberriExamen/src/test/java/com/gonzaloLecumberriExamen/GonzaloLecumberriExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzaloLecumberriExamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonzaloLecumberriExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
