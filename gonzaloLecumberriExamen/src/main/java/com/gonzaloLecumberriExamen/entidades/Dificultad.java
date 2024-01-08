66983ec14861df09077210db4eed6447c80ddab7
trontzal <trontzal@gmail.com>
2024-01-08 19:43:29 +0100

    Quito la entidad de dificultad

--- gonzaloLecumberriExamen/src/main/java/com/gonzaloLecumberriExamen/entidades/Dificultad.java
+++ gonzaloLecumberriExamen/src/main/java/com/gonzaloLecumberriExamen/entidades/Dificultad.java
@@ -1,15 +1,11 @@
 package com.gonzaloLecumberriExamen.entidades;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Data
+//@Builder
 public class Dificultad {
-	private Long id;
-	private String dificultad;
+//	private Long id;
+//	
+//	private String dificultad;
 }
