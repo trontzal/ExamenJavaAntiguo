b30e60fb6846e4dee1104cb05213d34d2d5a9d71
Curso <Curso@Gorbea01.ipartekaula.com>
2024-01-09 09:46:14 +0100

    formato

--- gonzaloLecumberriExamen/src/main/java/com/gonzaloLecumberriExamen/accesodatos/DaoRecetasJdbc.java
+++ gonzaloLecumberriExamen/src/main/java/com/gonzaloLecumberriExamen/accesodatos/DaoRecetasJdbc.java
@@ -8,13 +8,14 @@ import org.springframework.stereotype.Component;
 import com.gonzaloLecumberriExamen.entidades.Receta;
 
 @Component
-class DaoRecetasJdbc implements DaoReceta{
-	
+class DaoRecetasJdbc implements DaoReceta {
+
 	private static final String SQL_SELECT = "SELECT recetas.id, recetas.nombre, dificultades.dificultad as dificultad FROM recetas JOIN dificultades ON recetas.dificultad_id = dificultades.id";
 	private static final String SQL_INSERT = "INSERT INTO recetas (nombre, dificultad_id) VALUES (?, ?)";
+
 	@Autowired
 	private JdbcTemplate jdbc;
-	
+
 	@Override
 	public Iterable<Receta> obtenerTodos() {
 		return jdbc.query(SQL_SELECT, new BeanPropertyRowMapper<Receta>(Receta.class));
@@ -22,8 +23,8 @@ class DaoRecetasJdbc implements DaoReceta{
 
 	@Override
 	public Receta insertar(Receta receta) {
-	    jdbc.update(SQL_INSERT, receta.getNombre(), receta.getDificultad());
-	    return receta;
+		jdbc.update(SQL_INSERT, receta.getNombre(), receta.getDificultad());
+		return receta;
 	}
 
 }
