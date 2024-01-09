43e9dc954715f0b7756ebbb5758ede3e76dd5fc4
Curso <Curso@Gorbea01.ipartekaula.com>
2024-01-09 09:45:58 +0100

    comentario

--- gonzaloLecumberriExamen/src/main/java/com/gonzaloLecumberriExamen/accesodatos/DaoReceta.java
+++ gonzaloLecumberriExamen/src/main/java/com/gonzaloLecumberriExamen/accesodatos/DaoReceta.java
@@ -3,5 +3,5 @@ package com.gonzaloLecumberriExamen.accesodatos;
 import com.gonzaloLecumberriExamen.entidades.Receta;
 
 public interface DaoReceta extends Dao<Receta>{
-	
+	// LLamadas especificas para recetas
 }
